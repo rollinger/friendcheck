69995615c6893f7d7591f147c20f8e339c2d4de9
Philipp Rollinger <philipp.rollinger@gmail.com>
2019-02-04 20:24:20 +0100

    Model Definition Facebook App

--- facebook/models.py
+++ facebook/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import int_list_validator
+
+from friendcheck.users.models import User
+
 
 
 class Friend(models.Model):
-    pass
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    #name = CharField(_("Name of User"), blank=True, max_length=255)
+    owner       = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    fbid        = models.CharField(_('Facebook ID'), max_length=255)
+
+    description = models.TextField(_('Description'), null=True, blank=True,
+                max_length=2000)
+
+    avatar      = models.ImageField(_('Friend Avatar'),
+                upload_to='Friends/', null=True, blank=True)
+
+    created_at  = models.DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at  = models.DateTimeField(_('Updated at'), auto_now=True)
 
     #def get_absolute_url(self):
     #    return reverse("users:detail", kwargs={"username": self.username})
 
 class Datapoint(models.Model):
-    pass
+    owner       = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    datetime    = models.DateTimeField(_('Date Time'), auto_now_add=True)
+
+    fbid_data   = models.TextField(_('FB ID Rank Data'), validators=[int_list_validator])
