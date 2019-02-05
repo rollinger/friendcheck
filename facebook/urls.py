5a59f3c6ef7d1611c6a451652f54fe159b87d681
Philipp Rollinger <philipp.rollinger@gmail.com>
2019-02-05 11:47:51 +0100

    Change Model Attributes Migrations

---
+++ facebook/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+#from facebook.views import (
+#    user_list_view,
+#    user_redirect_view,
+#    user_update_view,
+#    user_detail_view,
+#)
+
+app_name = "facebook"
+urlpatterns = [
+    #path("", TemplateView.as_view(template_name="facebook/start.html"), name="facebook-start"),
+    #path("", view=user_list_view, name="list"),
+    #path("~redirect/", view=user_redirect_view, name="redirect"),
+    #path("~update/", view=user_update_view, name="update"),
+    #path("<str:username>/", view=user_detail_view, name="detail"),
+]
