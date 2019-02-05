c2afffe85919e3d357648381b959595760ae8ebb
Philipp Rollinger <philipp.rollinger@gmail.com>
2019-02-05 12:10:31 +0100

    Add Facebook Overview Page

--- facebook/urls.py
+++ facebook/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 #from facebook.views import (
+#   facebook_add_datapoint_view
 #    user_list_view,
 #    user_redirect_view,
 #    user_update_view,
@@ -9,7 +11,7 @@ from django.urls import path
 
 app_name = "facebook"
 urlpatterns = [
-    #path("", TemplateView.as_view(template_name="facebook/start.html"), name="facebook-start"),
+    path("", TemplateView.as_view(template_name="facebook/start.html"), name="overview"),
     #path("", view=user_list_view, name="list"),
     #path("~redirect/", view=user_redirect_view, name="redirect"),
     #path("~update/", view=user_update_view, name="update"),
